4571c964100a2333dc15a5eac990bd8678de281e
Eugene Gitonga <70195777+EugeneGitonga@users.noreply.github.com>
2022-06-20 01:41:04 +0300

    Pattern nested loops

---
+++ PatternPrinting.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace NumberPattern
+{
+class DisplayPattern
+   {
+static void Main(string[] args)
+      {
+int a , b ;
+
+for (a = 8 ; a >= 1 ; a--)
+{
+for (b = 1; b <= x ; b++)
+{
+Console.Write(b);
+       }
+Console.WriteLine();
+    }
+   }
+  }
+ }
